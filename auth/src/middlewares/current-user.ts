34297eefcff62c6fce60a1295b6d4a47e9e8ad55
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2020-07-27 21:17:01 -0400

    finished sign in, out, and current user auth service routes, added middleware to decode jwt into user payload and reject requests that require auth when a valid jwt is missing

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+// This is how you augment an existing type in typescript
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+
+    req.currentUser = payload;
+  } catch (err) {
+    console.log('Invalid JWT');
+  }
+
+  next();
+};
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,9 +2,9 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/signout', (req, res) => {
-  console.log('See ya!!');
-  res.send('See ya!');
+router.post('/api/users/signout', (req, res) => {
+  req.session = null;
+  res.send({});
 });
 
 export { router as signoutRouter };
