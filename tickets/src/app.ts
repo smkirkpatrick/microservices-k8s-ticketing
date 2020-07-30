3cf711384511c62053fdff05f1287c5fb3187f9f
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2020-07-30 16:16:22 -0400

    finished up TDD and build out of tickets service routes. updated ingress-nginx routing config and tested via postman

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,9 +3,16 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { errorHandler, NotFoundError } from '@smkirkpatrick-ticketing/common';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@smkirkpatrick-ticketing/common';
 
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -16,8 +23,12 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
