a3c8fc494c8f022c166ca6ff57b7c51080bfef30
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2020-07-31 23:14:44 -0400

    toy nats test project to experiment with nats streaming server (running in skaffold) and deal with concurrency issues

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,42 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable() // Get all available events in the past...
+    .setDurableName('accounting-service'); // ...since the last event processed by the _durable_ subscription! super rad.
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group', // durable subscription MUST have a queue group or it will be removed from NATS on a clean disconnect
+    options
+  );
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
+  });
+});
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
