7931700ad707a75a79a6e78885a2410fc6c1a9cf
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2020-08-02 15:46:46 -0400

    scaffolding for the orders service and k8s deployments

---
+++ orders/src/nats-wrapper.ts
@@ -0,0 +1,30 @@
+import nats, { Stan } from 'node-nats-streaming';
+
+class NatsWrapper {
+  private _client?: Stan;
+
+  get client() {
+    if (!this._client) {
+      throw new Error('Cannot access NATS client before connecting');
+    }
+    return this._client;
+  }
+
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise((resolve, reject) => {
+      console.log('Waiting for NATS to connect or error...');
+      this.client.on('connect', () => {
+        console.log('Connected to NATS');
+        resolve();
+      });
+      this.client.on('error', (err) => {
+        console.error('Failed to connect to NATS:', err);
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
