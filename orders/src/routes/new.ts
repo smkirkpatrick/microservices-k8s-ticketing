ce039256fb56d1d321e28b1558da03efeaa5dfde
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2020-08-03 11:39:18 -0400

    building our new order route and introduced Order model

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,27 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@smkirkpatrick-ticketing/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      // ^ This check can be skipped so that the orders service isn't subtly
+      // coupled with the specific implementation of the tickets service.
+      // Might *not* be a mongo database id...
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
