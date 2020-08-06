84be11a65d9680304b2288e3a535d932b178340f
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2020-08-05 22:55:06 -0400

    added tests to expiration complete listener in orders service

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,38 @@
+import { Message } from 'node-nats-streaming';
+import {
+  Listener,
+  Subjects,
+  ExpirationCompleteEvent,
+} from '@smkirkpatrick-ticketing/common';
+import { queueGroupName } from './queue-group-name';
+import { Order, OrderStatus } from '../../models/order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+
+export class ExpirationCompleteListener extends Listener<
+  ExpirationCompleteEvent
+> {
+  queueGroupName = queueGroupName;
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId).populate('ticket');
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+    await order.save();
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
+  }
+}
