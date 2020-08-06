0a0ce517ae99b0ef69a076218303c1e83e559a87
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2020-08-05 22:33:37 -0400

    use common module with ExpirationComplete event. Wired up job processing and delay. Emit expiration complete event when job ready to be processed

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -12,7 +12,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    await expirationQueue.add({ orderId: data.id });
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log(`Waiting ${delay} ms to process the job.`);
+
+    await expirationQueue.add(
+      { orderId: data.id },
+      {
+        delay,
+      }
+    );
 
     msg.ack();
   }
